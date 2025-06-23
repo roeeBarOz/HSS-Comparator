3af271dc5d7121ee9a6540f5ca498e98579026df
RoeeBarOz <broee@post.bgu.ac.il>
2025-06-23 22:40:19 +0300

    built basic structure of HSS

---
+++ HSS_schemes/Paillier_HSS.py
@@ -0,0 +1,22 @@
+
+
+def Setup(n):
+    
+
+def Input(pk, x):
+
+
+def Load(b, ek, i, id):
+
+
+def Add(b, ek, i1, i2, id):
+
+
+def Add(b, ek, m1, m2, id):
+
+
+def Mul(b, ek, i, m, id):
+
+
+def Output(b, ek, m, n_out, id):
+
