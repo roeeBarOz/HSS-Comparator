35e0a33fc7f78778583100a386385b4523696e6a
RoeeBarOz <broee@post.bgu.ac.il>
2025-06-24 12:16:13 +0300

    did half of Setup

--- HSS_schemes/Paillier_HSS.py
+++ HSS_schemes/Paillier_HSS.py
@@ -1,6 +1,34 @@
+import Encryptions.Paillier as Paillier
+from NTL_interfaces.ZZ_interface import {
+    zz_mod,
+    zz_div,
+    zz_pow,
+    zz_mul,
+    zz_random_smaller_than_n, 
+    zz_sub
+}
 
+kappa
+Bsk
+Bmsg
+pk_paillier
 
 def Setup(n):
+    # step 1
+    pk_paillier, sk = Paillier.Gen(n)
+    d = []
+    while(sk != "0"):
+        d.append(zz_mod(sk, Bsk))
+        sk = zz_div(sk, Bsk)
+    # step 2
+    random_range = zz_mul(zz_pow("2", kappa), Bsk)
+    d_0 = []
+    d_1 = []
+    for digit in d:
+        digit_1 = zz_random_smaller_than_n(random_range)
+        d_1.append(digit_1)
+        d_0.append(zz_sub(digit_1, digit))
+    # step 3
     
 
 def Input(pk, x):
